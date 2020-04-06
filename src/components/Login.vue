0650a7af37651a3c5315ba8c4cabfded8cc08660
alen9968 <xwzj0608@qq.com>
2020-04-06 11:14:31 +0800

    fix

---
+++ src/components/Login.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <div class="avatar">
+        <img src="../assets/alen.jpg" />
+      </div>
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="form">
+        <el-form-item prop="username" >
+          <el-input v-model="loginForm.username" prefix-icon="el-icon-search"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="loginForm.password" ></el-input>
+        </el-form-item>
+        <el-form-item class="btns" >
+          <el-button size="small" type="primary" @click="login">登录</el-button>
+          <el-button  size="small" type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'alen',
+        password: '123'
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      console.info(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        console.info(valid)
+        console.info(this)
+        if (!valid) return
+        const res = await this.axios.post('login', this.loginForm)
+        if (res.data.meta.status !== 200) return this.$message.err('Login failed!')
+        this.$message.success('Login success!')
+      })
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.avatar {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  padding: 10px;
+  border-radius: 50%;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  background-color: #33efee;
+  transform: translate(-50%, -50%);
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+}
+.form {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// import { Message } from 'element-ui'
+// Vue.prototype.$message = Message
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+
+Vue.use(VueAxios, axios)
+// axios.defaults.baseURL = 'http'
+// Vue.proptype.$http = axios
 
 Vue.config.productionTip = false
 
